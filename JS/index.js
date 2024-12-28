57784a43b3c5d230f6df9755908f9f90aaba0d69
ALEX.V <trofim.ivanchenko@nordclan.com>
2024-12-28 13:17:46 +0300

    Info block adaptive

--- JS/index.js
+++ JS/index.js
@@ -7,3 +7,25 @@ const openNav = () => {
   }
 }
 openNav()
+
+const swiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  slidesPerView: 1,
+  spaceBetween: 100,
+})
